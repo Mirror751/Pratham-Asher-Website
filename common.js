0bfcc94f170b44364189e63ee1b473607d2b53f5
Pratham Asher <168620496+Mirror751@users.noreply.github.com>
2025-03-09 14:39:51 -0700

    Add files via upload

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 function check_Location(name) {
-  return window.location.href.includes(name);
+    return window.location.href.includes(name);
 }
 
 console.log(window.location.href);
@@ -8,21 +8,21 @@ const navbar = `<div>
           <h3 class="float-md-start mb-0">Pratham Asher</h3>
           <nav class="nav nav-masthead justify-content-center float-md-end">
             <a
-              class="nav-link fw-bold py-1 px-0 ${(check_Location("index") || window.location.href == ("https://mirror751.github.io/Pratham-Asher-Website/")) ? "active" : ""}"
-              ${check_Location("index") ? `aria-current="page"` : ""}
+              class="nav-link fw-bold py-1 px-0 ${(check_Location("index") || window.location.href == ("https://mirror751.github.io/Pratham-Asher-Website/")) && "active"}"
+              ${check_Location("index") && `aria-current="page"`}
               href="./index.html"
               >Home</a
             >
-            <a class="nav-link fw-bold py-1 px-0 ${check_Location("badges") ? "active" : ""}"
-            ${check_Location("badges") ? `aria-current="page"` : ""} 
+            <a class="nav-link fw-bold py-1 px-0 ${check_Location("badges") && "active"}"
+            ${check_Location("badges") && `aria-current="page"`} 
             href="./badges.html"
               >Badges</a
             >
-            <a class="nav-link fw-bold py-1 px-0 ${check_Location("certifications") ? "active" : ""}" 
-            ${check_Location("certifications") ? `aria-current="page"` : ""}
+            <a class="nav-link fw-bold py-1 px-0 ${check_Location("certifications") && "active"}" 
+            ${check_Location("certifications") && `aria-current="page"`}
             href="./certifications.html">Certifications</a>
-            <a class="nav-link fw-bold py-1 px-0 ${check_Location("contact") ? "active" : ""}" 
-            ${check_Location("contact") ? `aria-current="page"` : ""}
+            <a class="nav-link fw-bold py-1 px-0 ${check_Location("contact") && "active"}" 
+            ${check_Location("contact") && `aria-current="page"`}
             href="./contact.html">Contact</a>
           </nav>
         </div>`;
